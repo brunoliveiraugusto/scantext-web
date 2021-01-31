96da1307aef8e477fb639c01214ec5be54b1d5cd
suporte <suporte@STFBHZC044043-L>
2021-01-31 15:38:40 -0300

    Criado método para ler imagem selecionada e retornar base64.

--- ScanTextApp/src/app/home-scan/home-scan.component.ts
+++ ScanTextApp/src/app/home-scan/home-scan.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeScanComponent implements OnInit {
 
+  public base64: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private selecionarImagem(event) {
+    if(event.target.files != null && event.target.files.length > 0) {
+      let base64;
+      var reader = new FileReader();
+      reader.onloadend = (e) => {
+        base64 = reader.result as string;
+        this.setBase64(base64);
+      }
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  public setBase64(base64: string) {
+    this.base64 = base64;
   }
 
 }
