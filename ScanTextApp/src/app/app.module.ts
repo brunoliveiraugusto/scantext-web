e960902f1466e5b5d0d02fd76df20ae5be7ab6ee
suporte <suporte@STFBHZC044043-L>
2021-01-31 13:59:21 -0300

    Criado componente home-scan

--- ScanTextApp/src/app/app.module.ts
+++ ScanTextApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeScanComponent } from './home-scan/home-scan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScanComponent
   ],
   imports: [
     BrowserModule,
---
+++ ScanTextApp/src/app/home-scan/home-scan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-scan',
+  templateUrl: './home-scan.component.html',
+  styleUrls: ['./home-scan.component.css']
+})
+export class HomeScanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
