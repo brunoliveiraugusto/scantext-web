9924a158f243341cb18081830e0d8dccd5ef9a7b
suporte <suporte@STFBHZC044043-L>
2021-02-05 20:17:10 -0300

    Criado método base getAll.

--- ScanTextApp/src/app/services/base-http.service.ts
+++ ScanTextApp/src/app/services/base-http.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ScanResponse } from '../models/scan-reponse';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class BaseHttpService<T> {
 
   protected _http: HttpClient;
   protected resource: string;
-  protected baseUrl: string;
+  protected baseUrl: string = environment.apiUrl;
 
   constructor(private http: HttpClient, resource: string) { 
     this._http = http;
@@ -24,4 +25,8 @@ export class BaseHttpService<T> {
   public getById(action: string = '', id: string): Observable<ScanResponse<T>> {
     return this._http.get<ScanResponse<T>>(`${this.baseUrl}/${this.resource}/${action}${id}`);
   }
+
+  public getAll(action: string = ''): Observable<ScanResponse<Array<T>>> {
+    return this._http.get<ScanResponse<Array<T>>>(`${this.baseUrl}/${this.resource}/${action}`);
+}
 }
