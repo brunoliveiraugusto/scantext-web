4a3bff050afec10237b8e4a91ecf202fee8a9093
suporte <suporte@STFBHZC044043-L>
2021-02-05 20:17:42 -0300

    Adicionado módulo HttpClient.

--- ScanTextApp/src/app/app.module.ts
+++ ScanTextApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeScanComponent } from './home-scan/home-scan.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HomeScanComponent } from './home-scan/home-scan.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
