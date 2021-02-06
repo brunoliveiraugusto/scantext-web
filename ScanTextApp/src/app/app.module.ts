14851b5a0f3d8428eb875f3288c67423f9d91eb0
suporte <suporte@STFBHZC044043-L>
2021-02-05 21:03:50 -0300

    Adicionado componente ng-multiselect-dropdown ao projeto.

--- ScanTextApp/src/app/app.module.ts
+++ ScanTextApp/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeScanComponent } from './home-scan/home-scan.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgMultiSelectDropDownModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
