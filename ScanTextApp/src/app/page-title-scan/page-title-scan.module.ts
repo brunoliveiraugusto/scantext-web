4ea43a773f941aca8e47d965d80638b1005c3a47
suporte <suporte@STFBHZC044043-L>
2021-02-13 15:48:14 -0300

    Criado módulo para o componente page-title.

---
+++ ScanTextApp/src/app/page-title-scan/page-title-scan.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageTitleScanComponent } from './page-title-scan.component';
+
+
+@NgModule({
+  declarations: [
+    PageTitleScanComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PageTitleScanComponent
+  ]
+})
+export class PageTitleScanModule { }
