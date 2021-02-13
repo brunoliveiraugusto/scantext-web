020aff60da4c64f10183264874c6ad500d09e651
suporte <suporte@STFBHZC044043-L>
2021-02-13 00:44:10 -0300

    Alterado o tipo da propriedade size.

--- ScanTextApp/src/app/models/imagem.ts
+++ ScanTextApp/src/app/models/imagem.ts
@@ -3,7 +3,7 @@ import { Linguagem } from "./linguagem";
 export class Imagem {
     id: string;
     nome: string;
-    size: string;
+    size: number;
     formato: string;
     base64: string;
     texto: string;
