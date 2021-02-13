616ce6ad58aa26217a770cf2c9031d822b10eb20
suporte <suporte@STFBHZC044043-L>
2021-02-13 15:20:22 -0300

    Criado componente para apresentação das imagens processadas.

---
+++ ScanTextApp/src/app/components/imagem-processada-scan/imagem-processada-scan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imagem-processada-scan',
+  templateUrl: './imagem-processada-scan.component.html',
+  styleUrls: ['./imagem-processada-scan.component.css']
+})
+export class ImagemProcessadaScanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
