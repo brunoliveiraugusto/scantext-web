b0f4f374fe9bd32e6a4420998d7e652892c34622
suporte <suporte@STFBHZC044043-L>
2021-02-13 15:22:54 -0300

    Criação de rotas para os componentes da aplicação.

--- ScanTextApp/src/app/app-routing.module.ts
+++ ScanTextApp/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeScanComponent } from './home-scan/home-scan.component';
+import { ImagemProcessadaScanComponent } from './components/imagem-processada-scan/imagem-processada-scan.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    component: HomeScanComponent
+  }, 
+  {
+    path: '/imagens-digitalizadas',
+    component: ImagemProcessadaScanComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
