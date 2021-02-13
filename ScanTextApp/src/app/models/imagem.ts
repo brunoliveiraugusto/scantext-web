745e61047c692a85df5fba96bef28c58da8a91ee
suporte <suporte@STFBHZC044043-L>
2021-02-12 23:42:13 -0300

    Adição de campos ao model Imagem.

--- ScanTextApp/src/app/models/imagem.ts
+++ ScanTextApp/src/app/models/imagem.ts
@@ -2,6 +2,9 @@ import { Linguagem } from "./linguagem";
 
 export class Imagem {
     id: string;
+    nome: string;
+    size: string;
+    formato: string;
     base64: string;
     texto: string;
     linguagem: Linguagem;
