0e133cbf330c7765b0b804ed31e64894c11959cc
suporte <suporte@STFBHZC044043-L>
2021-02-20 01:57:38 -0300

    Inclusão de novas rotas para os componentes que foram criados.

--- ScanTextApp/src/app/app-routing.module.ts
+++ ScanTextApp/src/app/app-routing.module.ts
@@ -2,23 +2,30 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProcessarImagemScanComponent } from './components/imagem/processar-imagem-scan/processar-imagem-scan.component';
 import { ConsultaImagemProcessadaScanComponent } from './components/imagem/consulta-imagem-processada-scan/consulta-imagem-processada-scan.component';
+import { LoginScanComponent } from './login/components/login-scan/login-scan.component';
+import { AuthGuard } from './login/shared/auth.guard';
+import { AuthenticationScanComponent } from './login/components/authentication-scan/authentication-scan.component';
+import { HomeScanComponent } from './home/home-scan/home-scan.component';
 
 const routes: Routes = [
   {
-    path: 'processar-imagem',
-    component: ProcessarImagemScanComponent
+    path: '',
+    component: HomeScanComponent,
+    children: [
+      { path: 'processar-imagem', component: ProcessarImagemScanComponent },
+      { path: 'processar-imagem/:id', component: ProcessarImagemScanComponent },
+      { path: 'consulta-imagens-processadas', component: ConsultaImagemProcessadaScanComponent },
+      { path: '**', component: ProcessarImagemScanComponent }
+    ],
+    canActivate: [AuthGuard]
   },
   {
-    path: 'processar-imagem/:id',
-    component: ProcessarImagemScanComponent
-  },
-  {
-    path: 'consulta-imagens-processadas',
-    component: ConsultaImagemProcessadaScanComponent
-  },
-  {
-    path: '**',
-    component: ProcessarImagemScanComponent
+    path: '',
+    component: AuthenticationScanComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+      { path: 'login', component: LoginScanComponent }
+    ]
   } 
 ];
 
