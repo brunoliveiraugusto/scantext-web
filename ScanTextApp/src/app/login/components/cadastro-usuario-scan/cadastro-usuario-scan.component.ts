c6805da57eec05dd92b2a01b72844d2e419a5f17
suporte <suporte@STFBHZC044043-L>
2021-02-27 13:30:52 -0300

    Criada implementação para cadastrar um novo usuário e verificar se o mesmo já existe.

--- ScanTextApp/src/app/login/components/cadastro-usuario-scan/cadastro-usuario-scan.component.ts
+++ ScanTextApp/src/app/login/components/cadastro-usuario-scan/cadastro-usuario-scan.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertService } from 'ngx-alerts';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-cadastro-usuario-scan',
@@ -8,13 +12,85 @@ import { Router } from '@angular/router';
 })
 export class CadastroUsuarioScanComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  usuario: Usuario;
+  loading: boolean = false;
+  indicaUsuarioExistente: boolean = false;
+
+  constructor(private router: Router, private usuarioService: UsuarioService, private alertService: AlertService) { 
+    this.usuario = new Usuario();
+  }
 
   ngOnInit() {
   }
 
   navigateTo(route: string) {
     this.router.navigate([route]);
+  }
+
+  indicaUsuarioValido() {
+    if(isNullOrUndefined(this.usuario.username)) {
+      this.alertService.warning("O campo Usuário é obrigatório.");
+      return false;
+    }
+
+    if(isNullOrUndefined(this.usuario.password)) {
+      this.alertService.warning("O campo Senha é obrigatório.");
+      return false;
+    }
+
+    if(this.usuario.password.length < 8) {
+      this.alertService.warning("O campo Senha deve conter no mínimo 8 caracteres.");
+      return false;
+    }
+
+    if(isNullOrUndefined(this.usuario.email)) {
+      this.alertService.warning("O campo E-mail é obrigatório.");
+      return false;
+    }
+
+    if(isNullOrUndefined(this.usuario.dataNascimento)) {
+      this.alertService.warning("O campo Data de Nascimento é obrigatório.");
+      return false;
+    }
+
+    return true;
+  }
+
+  criarUsuario() {
+    if(this.indicaUsuarioValido()) {
+      this.Loading();
+      this.usuarioService.post('', this.usuario) 
+      .subscribe((res) => {
+        this.alertService.success("Usuário criado com sucesso.");
+        this.Loading();
+        this.navigateTo('login');
+      }, (err) => {
+        this.alertService.danger("Erro ao tentar criar um novo usuário.");
+        this.Loading();
+      });
+    }
+  }
+
+  verificarUsuarioExistente() {
+    if(isNullOrUndefined(this.usuario.username) || this.usuario.username == "") {
+      this.indicaUsuarioExistente = false;
+      return;
+    }
+
+    const query = this.getQuery();
+    this.usuarioService.get(`verificar-usuario-existente${query}`)
+    .subscribe((res) => {
+      let indicaUsuarioExistente = res as any;
+      this.indicaUsuarioExistente = indicaUsuarioExistente;
+    });
+  }
+
+  getQuery() {
+    return '?username='.concat(this.usuario.username);
+  }
+
+  public Loading() {
+    this.loading = !this.loading;
   }
 
 }
