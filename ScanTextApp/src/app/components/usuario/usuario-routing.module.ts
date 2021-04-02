7f797a40e6e9286ec7dd4ce2672f008c1901f9e7
suporte <suporte@STFBHZC044043-L>
2021-04-02 14:19:03 -0300

    Adicionado rota para o componente de perfil.

--- ScanTextApp/src/app/components/usuario/usuario-routing.module.ts
+++ ScanTextApp/src/app/components/usuario/usuario-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainUsuarioScanComponent } from './main-usuario-scan/main-usuario-scan.component';
+import { PerfilUsuarioScanComponent } from './perfil-usuario-scan/perfil-usuario-scan.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainUsuarioScanComponent,
+    children: [
+      { path: 'perfil', component: PerfilUsuarioScanComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
