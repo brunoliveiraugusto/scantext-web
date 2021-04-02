47c44e09d832da52ac9e60e1ecb7d40b93a65ee9
suporte <suporte@STFBHZC044043-L>
2021-04-02 13:46:14 -0300

    Renomeado componente de perfil.

---
+++ ScanTextApp/src/app/components/configuração/perfil-usuario-scan/perfil-usuario-scan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-usuario-scan',
+  templateUrl: './perfil-usuario-scan.component.html',
+  styleUrls: ['./perfil-usuario-scan.component.css']
+})
+export class PerfilUsuarioScanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
