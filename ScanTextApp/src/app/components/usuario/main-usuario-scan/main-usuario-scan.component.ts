c2e073723bb2696ad3f589efa1c769ecceef1fe5
suporte <suporte@STFBHZC044043-L>
2021-04-02 13:49:06 -0300

    Criado componente main-usuario.

---
+++ ScanTextApp/src/app/components/usuario/main-usuario-scan/main-usuario-scan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-usuario-scan',
+  templateUrl: './main-usuario-scan.component.html',
+  styleUrls: []
+})
+export class MainUsuarioScanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
