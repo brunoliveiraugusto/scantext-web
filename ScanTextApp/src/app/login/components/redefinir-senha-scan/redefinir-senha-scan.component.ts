97c0ef7a53e4e1405a1bfd45256525bd70f8fa5d
suporte <suporte@STFBHZC044043-L>
2021-04-30 21:15:41 -0300

    Criação de componente para redefinição de senha.

---
+++ ScanTextApp/src/app/login/components/redefinir-senha-scan/redefinir-senha-scan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redefinir-senha-scan',
+  templateUrl: './redefinir-senha-scan.component.html',
+  styleUrls: ['./redefinir-senha-scan.component.css']
+})
+export class RedefinirSenhaScanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
